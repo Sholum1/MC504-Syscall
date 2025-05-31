d2ee2859210e90d2468a98da3c6265869da2dd7c
The_Borts <bortolettolucas2005@gmail.com>
2025-05-30 21:28:19 -0300

    Adicionando código teste do syscall

---
+++ teste_syscall.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+
+#define sys_sleep_proc 468
+
+int main() {
+    
+    long ret;
+    unsigned int seconds = 5;
+
+    printf("Sleeping for %u seconds...\n", seconds);
+    
+    // Call the syscall to sleep for a specified number of seconds
+    ret = syscall(sys_sleep_proc, seconds);
+
+    // Check if the syscall was successful
+    if (ret == -1) {
+        perror("syscall failed");
+        return 1;
+    }
+
+    printf("Slept for %u seconds\n", seconds);
+    return 0;
+
+    
+}
